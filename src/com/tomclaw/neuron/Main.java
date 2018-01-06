260e55d9a1538aac397230297b5605120117c5cf
Igor Solkin <inbox@tomclaw.com>
2018-01-07 02:25:30 +0300

    Fix params for success couching result

--- src/com/tomclaw/neuron/Main.java
+++ src/com/tomclaw/neuron/Main.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class Main {
 
+    private static DecimalFormat df = new DecimalFormat("#.####");
+
     public static void main(String[] args) {
         List<double[]> sets = new ArrayList<>();
         sets.add(new double[]{0, 0, 0});
@@ -21,17 +23,19 @@ public class Main {
 
         OutputNeuron o1 = new OutputNeuron();
 
-        i1.addReceiver(h1, 0.45);
-        i1.addReceiver(h2, 0.78);
+        i1.addReceiver(h1, 0.6);
+        i1.addReceiver(h2, 1.1);
 
-        i2.addReceiver(h1, -0.12);
-        i2.addReceiver(h2, 0.13);
+        i2.addReceiver(h1, 0.6);
+        i2.addReceiver(h2, 1.1);
 
-        h1.addReceiver(o1, 1.5);
-        h2.addReceiver(o1, -2.3);
+        h1.addReceiver(o1, -2);
+        h2.addReceiver(o1, 1.1);
 
-        for (int epoch = 0; epoch < 20; epoch++) {
+        boolean hasErrors;
+        for (int epoch = 0; epoch < 1000; epoch++) {
             System.out.println("--- epoch " + (epoch + 1) + " ---");
+            hasErrors = false;
             for (double[] set : sets) {
                 double val1 = set[0];
                 double val2 = set[1];
@@ -42,16 +46,44 @@ public class Main {
 
                 Double output = o1.getOutput();
                 if (output == null) {
-                    System.out.println("no output");
+                    System.err.println("no output");
+                    return;
                 } else {
-                    double error = Math.pow(ideal - output, sets.size()) / 1;
+                    long rounded = Math.round(output);
+                    double error = Math.pow(ideal - output, 2) / sets.size();
 
-                    DecimalFormat df = new DecimalFormat("#.####");
-                    System.out.println(val1 + " ^ " + val2 + " = " + df.format(output) + ", error: " + df.format(error));
+                    System.out.println(val1 + " ^ " + val2 + " = " + rounded + " (" + df.format(output) + "), error: " + df.format(error));
+
+                    if (rounded != ideal) {
+                        hasErrors = true;
+                    }
 
                     o1.couch(ideal);
                 }
             }
+            if (!hasErrors) {
+                System.out.println("\n\ncouched by " + (epoch + 1) + " epochs");
+                onCouched(i1, i2, o1, sets);
+                break;
+            }
+        }
+
+    }
+
+    private static void onCouched(InputNeuron i1, InputNeuron i2, OutputNeuron o1, List<double[]> sets) {
+        for (double[] set : sets) {
+            double val1 = set[0];
+            double val2 = set[1];
+            double ideal = set[2];
+
+            i1.emit(val1);
+            i2.emit(val2);
+
+            Double output = o1.getOutput();
+            long rounded = Math.round(output);
+            double error = Math.pow(ideal - output, 2) / sets.size();
+
+            System.out.println(val1 + " & " + val2 + " = " + rounded + " (" + df.format(output) + "), error: " + df.format(error));
         }
     }
 }
